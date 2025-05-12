dcf45c21021923d2ad35bc58a36198c744f8dde5
villagerunknown <VillagerUnknown@proton.me>
2025-05-12 01:08:31 -0400

    Cleaning up

--- src/main/java/me/villagerunknown/coinbanks/Coinbanks.java
+++ src/main/java/me/villagerunknown/coinbanks/Coinbanks.java
@@ -1,9 +1,10 @@
 package me.villagerunknown.coinbanks;
 
-import me.villagerunknown.coinbanks.feature.CoinBankBlocksFeatureLoader;
+import me.villagerunknown.coinbanks.feature.CoinBankBlocksFeature;
 import me.villagerunknown.platform.Platform;
 import me.villagerunknown.platform.PlatformMod;
 import me.villagerunknown.platform.manager.featureManager;
+import me.villagerunknown.villagercoin.Villagercoin;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 
@@ -15,11 +16,14 @@ public class Coinbanks implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
-		// # Initialize mod with Platform
+		// # Load Villager Coin
+		Villagercoin.load();
+		
+		// # Initialize addon mod with Platform
 		Platform.init_mod( MOD );
 		
 		// # Activate Features
-		featureManager.addFeature( "coinBankBlocksFeatureLoader", CoinBankBlocksFeatureLoader::execute );
+		featureManager.addFeature( "coin-bank-blocks", CoinBankBlocksFeature::execute );
 		
 		// # Load Features
 		featureManager.loadFeatures();
--- src/main/java/me/villagerunknown/coinbanks/block/CoinBankBlocks.java
+++ src/main/java/me/villagerunknown/coinbanks/block/CoinBankBlocks.java
@@ -1,6 +1,6 @@
 package me.villagerunknown.coinbanks.block;
 
-import me.villagerunknown.coinbanks.feature.CoinBankBlocksFeatureLoader;
+import me.villagerunknown.coinbanks.feature.CoinBankBlocksFeature;
 import net.minecraft.block.Block;
 
 public class CoinBankBlocks {
@@ -74,71 +74,71 @@ public class CoinBankBlocks {
 	public CoinBankBlocks(){}
 	
 	static{
-		TERRACOTTA_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "terracotta_coin_bank" );
-		WHITE_TERRACOTTA_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "white_terracotta_coin_bank" );
-		ORANGE_TERRACOTTA_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "orange_terracotta_coin_bank" );
-		MAGENTA_TERRACOTTA_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "magenta_terracotta_coin_bank" );
-		LIGHT_BLUE_TERRACOTTA_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "light_blue_terracotta_coin_bank" );
-		YELLOW_TERRACOTTA_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "yellow_terracotta_coin_bank" );
-		LIME_TERRACOTTA_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "lime_terracotta_coin_bank" );
-		PINK_TERRACOTTA_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "pink_terracotta_coin_bank" );
-		GRAY_TERRACOTTA_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "gray_terracotta_coin_bank" );
-		LIGHT_GRAY_TERRACOTTA_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "light_gray_terracotta_coin_bank" );
-		CYAN_TERRACOTTA_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "cyan_terracotta_coin_bank" );
-		PURPLE_TERRACOTTA_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "purple_terracotta_coin_bank" );
-		BLUE_TERRACOTTA_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "blue_terracotta_coin_bank" );
-		BROWN_TERRACOTTA_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "brown_terracotta_coin_bank" );
-		GREEN_TERRACOTTA_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "green_terracotta_coin_bank" );
-		RED_TERRACOTTA_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "red_terracotta_coin_bank" );
-		BLACK_TERRACOTTA_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "black_terracotta_coin_bank" );
+		TERRACOTTA_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "terracotta_coin_bank" );
+		WHITE_TERRACOTTA_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "white_terracotta_coin_bank" );
+		ORANGE_TERRACOTTA_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "orange_terracotta_coin_bank" );
+		MAGENTA_TERRACOTTA_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "magenta_terracotta_coin_bank" );
+		LIGHT_BLUE_TERRACOTTA_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "light_blue_terracotta_coin_bank" );
+		YELLOW_TERRACOTTA_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "yellow_terracotta_coin_bank" );
+		LIME_TERRACOTTA_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "lime_terracotta_coin_bank" );
+		PINK_TERRACOTTA_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "pink_terracotta_coin_bank" );
+		GRAY_TERRACOTTA_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "gray_terracotta_coin_bank" );
+		LIGHT_GRAY_TERRACOTTA_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "light_gray_terracotta_coin_bank" );
+		CYAN_TERRACOTTA_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "cyan_terracotta_coin_bank" );
+		PURPLE_TERRACOTTA_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "purple_terracotta_coin_bank" );
+		BLUE_TERRACOTTA_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "blue_terracotta_coin_bank" );
+		BROWN_TERRACOTTA_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "brown_terracotta_coin_bank" );
+		GREEN_TERRACOTTA_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "green_terracotta_coin_bank" );
+		RED_TERRACOTTA_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "red_terracotta_coin_bank" );
+		BLACK_TERRACOTTA_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "black_terracotta_coin_bank" );
 		
-		COPPER_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "copper_coin_bank" );
-		IRON_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "iron_coin_bank" );
-		GOLD_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "gold_coin_bank" );
-		DIAMOND_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "diamond_coin_bank" );
-		EMERALD_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "emerald_coin_bank" );
-		NETHERITE_COIN_BANK = CoinBankBlocksFeatureLoader.registerToughFireproofJarCoinBankBlock( "netherite_coin_bank" );
+		COPPER_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "copper_coin_bank" );
+		IRON_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "iron_coin_bank" );
+		GOLD_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "gold_coin_bank" );
+		DIAMOND_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "diamond_coin_bank" );
+		EMERALD_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "emerald_coin_bank" );
+		NETHERITE_COIN_BANK = CoinBankBlocksFeature.registerToughFireproofJarCoinBankBlock( "netherite_coin_bank" );
 		
-		AMETHYST_COIN_BANK = CoinBankBlocksFeatureLoader.registerJarCoinBankBlock( "amethyst_coin_bank" );
+		AMETHYST_COIN_BANK = CoinBankBlocksFeature.registerJarCoinBankBlock( "amethyst_coin_bank" );
 		
-		OBSIDIAN_COIN_BANK = CoinBankBlocksFeatureLoader.registerToughJarCoinBankBlock( "obsidian_coin_bank" );
-		CRYING_OBSIDIAN_COIN_BANK = CoinBankBlocksFeatureLoader.registerToughJarCoinBankBlock( "crying_obsidian_coin_bank" );
+		OBSIDIAN_COIN_BANK = CoinBankBlocksFeature.registerToughJarCoinBankBlock( "obsidian_coin_bank" );
+		CRYING_OBSIDIAN_COIN_BANK = CoinBankBlocksFeature.registerToughJarCoinBankBlock( "crying_obsidian_coin_bank" );
 		
-		COLD_PIG_COIN_BANK = CoinBankBlocksFeatureLoader.registerPiggyBankBlock( "cold_piggy_bank" );
-		TEMPERATE_PIG_COIN_BANK = CoinBankBlocksFeatureLoader.registerPiggyBankBlock( "temperate_piggy_bank" );
-		WARM_PIG_COIN_BANK = CoinBankBlocksFeatureLoader.registerPiggyBankBlock( "warm_piggy_bank" );
+		COLD_PIG_COIN_BANK = CoinBankBlocksFeature.registerPiggyBankBlock( "cold_piggy_bank" );
+		TEMPERATE_PIG_COIN_BANK = CoinBankBlocksFeature.registerPiggyBankBlock( "temperate_piggy_bank" );
+		WARM_PIG_COIN_BANK = CoinBankBlocksFeature.registerPiggyBankBlock( "warm_piggy_bank" );
 		
-		PIGLIN_COIN_BANK = CoinBankBlocksFeatureLoader.registerPiggyBankBlock( "piglin_bank" );
-		ZOMBIFIED_PIGLIN_COIN_BANK = CoinBankBlocksFeatureLoader.registerPiggyBankBlock( "zombified_piglin_bank" );
+		PIGLIN_COIN_BANK = CoinBankBlocksFeature.registerPiggyBankBlock( "piglin_bank" );
+		ZOMBIFIED_PIGLIN_COIN_BANK = CoinBankBlocksFeature.registerPiggyBankBlock( "zombified_piglin_bank" );
 		
-		WHITE_SHEEP_COIN_BANK = CoinBankBlocksFeatureLoader.registerSheepBankBlock( "white_sheep_bank" );
-		ORANGE_SHEEP_COIN_BANK = CoinBankBlocksFeatureLoader.registerSheepBankBlock( "orange_sheep_bank" );
-		MAGENTA_SHEEP_COIN_BANK = CoinBankBlocksFeatureLoader.registerSheepBankBlock( "magenta_sheep_bank" );
-		LIGHT_BLUE_SHEEP_COIN_BANK = CoinBankBlocksFeatureLoader.registerSheepBankBlock( "light_blue_sheep_bank" );
-		YELLOW_SHEEP_COIN_BANK = CoinBankBlocksFeatureLoader.registerSheepBankBlock( "yellow_sheep_bank" );
-		LIME_SHEEP_COIN_BANK = CoinBankBlocksFeatureLoader.registerSheepBankBlock( "lime_sheep_bank" );
-		PINK_SHEEP_COIN_BANK = CoinBankBlocksFeatureLoader.registerSheepBankBlock( "pink_sheep_bank" );
-		GRAY_SHEEP_COIN_BANK = CoinBankBlocksFeatureLoader.registerSheepBankBlock( "gray_sheep_bank" );
-		LIGHT_GRAY_SHEEP_COIN_BANK = CoinBankBlocksFeatureLoader.registerSheepBankBlock( "light_gray_sheep_bank" );
-		CYAN_SHEEP_COIN_BANK = CoinBankBlocksFeatureLoader.registerSheepBankBlock( "cyan_sheep_bank" );
-		PURPLE_SHEEP_COIN_BANK = CoinBankBlocksFeatureLoader.registerSheepBankBlock( "purple_sheep_bank" );
-		BLUE_SHEEP_COIN_BANK = CoinBankBlocksFeatureLoader.registerSheepBankBlock( "blue_sheep_bank" );
-		BROWN_SHEEP_COIN_BANK = CoinBankBlocksFeatureLoader.registerSheepBankBlock( "brown_sheep_bank" );
-		GREEN_SHEEP_COIN_BANK = CoinBankBlocksFeatureLoader.registerSheepBankBlock( "green_sheep_bank" );
-		RED_SHEEP_COIN_BANK = CoinBankBlocksFeatureLoader.registerSheepBankBlock( "red_sheep_bank" );
-		BLACK_SHEEP_COIN_BANK = CoinBankBlocksFeatureLoader.registerSheepBankBlock( "black_sheep_bank" );
+		WHITE_SHEEP_COIN_BANK = CoinBankBlocksFeature.registerSheepBankBlock( "white_sheep_bank" );
+		ORANGE_SHEEP_COIN_BANK = CoinBankBlocksFeature.registerSheepBankBlock( "orange_sheep_bank" );
+		MAGENTA_SHEEP_COIN_BANK = CoinBankBlocksFeature.registerSheepBankBlock( "magenta_sheep_bank" );
+		LIGHT_BLUE_SHEEP_COIN_BANK = CoinBankBlocksFeature.registerSheepBankBlock( "light_blue_sheep_bank" );
+		YELLOW_SHEEP_COIN_BANK = CoinBankBlocksFeature.registerSheepBankBlock( "yellow_sheep_bank" );
+		LIME_SHEEP_COIN_BANK = CoinBankBlocksFeature.registerSheepBankBlock( "lime_sheep_bank" );
+		PINK_SHEEP_COIN_BANK = CoinBankBlocksFeature.registerSheepBankBlock( "pink_sheep_bank" );
+		GRAY_SHEEP_COIN_BANK = CoinBankBlocksFeature.registerSheepBankBlock( "gray_sheep_bank" );
+		LIGHT_GRAY_SHEEP_COIN_BANK = CoinBankBlocksFeature.registerSheepBankBlock( "light_gray_sheep_bank" );
+		CYAN_SHEEP_COIN_BANK = CoinBankBlocksFeature.registerSheepBankBlock( "cyan_sheep_bank" );
+		PURPLE_SHEEP_COIN_BANK = CoinBankBlocksFeature.registerSheepBankBlock( "purple_sheep_bank" );
+		BLUE_SHEEP_COIN_BANK = CoinBankBlocksFeature.registerSheepBankBlock( "blue_sheep_bank" );
+		BROWN_SHEEP_COIN_BANK = CoinBankBlocksFeature.registerSheepBankBlock( "brown_sheep_bank" );
+		GREEN_SHEEP_COIN_BANK = CoinBankBlocksFeature.registerSheepBankBlock( "green_sheep_bank" );
+		RED_SHEEP_COIN_BANK = CoinBankBlocksFeature.registerSheepBankBlock( "red_sheep_bank" );
+		BLACK_SHEEP_COIN_BANK = CoinBankBlocksFeature.registerSheepBankBlock( "black_sheep_bank" );
 		
-		COW_COIN_BANK = CoinBankBlocksFeatureLoader.registerCowBankBlock( "cow_bank" );
-		RED_MOOSHROOM_COIN_BANK = CoinBankBlocksFeatureLoader.registerCowBankBlock( "red_mooshroom_bank" );
-		BROWN_MOOSHROOM_COIN_BANK = CoinBankBlocksFeatureLoader.registerCowBankBlock( "brown_mooshroom_bank" );
+		COW_COIN_BANK = CoinBankBlocksFeature.registerCowBankBlock( "cow_bank" );
+		RED_MOOSHROOM_COIN_BANK = CoinBankBlocksFeature.registerCowBankBlock( "red_mooshroom_bank" );
+		BROWN_MOOSHROOM_COIN_BANK = CoinBankBlocksFeature.registerCowBankBlock( "brown_mooshroom_bank" );
 		
-		BEE_COIN_BANK = CoinBankBlocksFeatureLoader.registerBeeBankBlock( "bee_bank" );
-		ANGRY_BEE_COIN_BANK = CoinBankBlocksFeatureLoader.registerBeeBankBlock( "angry_bee_bank" );
+		BEE_COIN_BANK = CoinBankBlocksFeature.registerBeeBankBlock( "bee_bank" );
+		ANGRY_BEE_COIN_BANK = CoinBankBlocksFeature.registerBeeBankBlock( "angry_bee_bank" );
 		
-		RAVAGER_COIN_BANK = CoinBankBlocksFeatureLoader.registerRavagerBankBlock( "ravager_bank" );
+		RAVAGER_COIN_BANK = CoinBankBlocksFeature.registerRavagerBankBlock( "ravager_bank" );
 		
-		GUARDIAN_COIN_BANK = CoinBankBlocksFeatureLoader.registerGuardianBankBlock( "guardian_bank" );
-		ELDER_GUARDIAN_COIN_BANK = CoinBankBlocksFeatureLoader.registerGuardianBankBlock( "elder_guardian_bank" );
+		GUARDIAN_COIN_BANK = CoinBankBlocksFeature.registerGuardianBankBlock( "guardian_bank" );
+		ELDER_GUARDIAN_COIN_BANK = CoinBankBlocksFeature.registerGuardianBankBlock( "elder_guardian_bank" );
 	}
 
 }
